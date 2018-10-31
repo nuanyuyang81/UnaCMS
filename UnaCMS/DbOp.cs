37462d057d686325633bb60de8ca43193cc97aaa
weiwei <nuanyuyang81@qq.com>
2018-10-31 17:30:06 +0800

    in

--- UnaCMS/DbOp.cs
+++ UnaCMS/DbOp.cs
@@ -81,6 +81,46 @@ namespace UnaCMS
         #endregion
 
         #region 用户
+        /// <summary>
+        /// 注册用户
+        /// </summary>
+        /// <param name="idgroup"></param>
+        /// <param name="username"></param>
+        /// <param name="salt"></param>
+        /// <param name="password"></param>
+        /// <param name="email"></param>
+        /// <param name="regip"></param>
+        /// <returns></returns>
+        public static bool RegUser(int idgroup,string username,string salt,string password,string email,string regip)
+        {
+            string cmdline = "inesrt into [una].[user](idgroup,username,salt,password,email,regip) values(@idgroup,@username,@salt,@password,@email,@regip)";
+            using(SqlConnection conn = DbConn())
+            {
+                SqlCommand cmd = new SqlCommand(cmdline, conn);
+                cmd.Parameters.AddWithValue("@idgroup", idgroup);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@salt", salt);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@regip", regip);
+                try
+                {
+                    return cmd.ExecuteNonQuery()>0;
+                }
+                catch(SqlException ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        #endregion
+
+        #region 文章
+        #endregion
+
+        #region 频道
+        public static void AddChannel()
         #endregion
     }
 }
