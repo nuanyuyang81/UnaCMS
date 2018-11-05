b17ded2468bced3771663c3e189ac998b9165f84
weyin <nuanyuyang@hotmail.com>
2018-11-05 20:39:41 +0800

    文章状态

--- UnaUtility/ArticleStatus.cs
+++ UnaUtility/ArticleStatus.cs
@@ -5,9 +5,21 @@ namespace UnaUtility
 {
     public enum ArticleStatus
     {
+        /// <summary>
+        /// 正常
+        /// </summary>
         Normal = 0,
+        /// <summary>
+        /// 未审批
+        /// </summary>
         UnApprove = 1,
+        /// <summary>
+        /// 已审批
+        /// </summary>
         Approved = 2,
+        /// <summary>
+        /// 转载
+        /// </summary>
         Republish=3
     }
 }
